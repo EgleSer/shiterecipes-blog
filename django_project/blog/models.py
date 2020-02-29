22caa82abab0f5cdc39af34328b74382ad8bb7fd
egle <rappee@gmail.com>
2020-02-29 17:40:30 +0200

    Adding a feature to upload pictures in posts.

--- django_project/blog/models.py
+++ django_project/blog/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
 from django.urls import reverse
+from PIL import Image
 
 
 class Post(models.Model):
@@ -9,9 +10,21 @@ class Post(models.Model):
     content = models.TextField()
     date_posted = models.DateTimeField(default=timezone.now)
     author = models.ForeignKey(User, on_delete=models.CASCADE)
+    image = models.ImageField(default='default-food.jpg', upload_to='post_pics')
+    
 
     def __str__(self):
         return self.title
 
     def get_absolute_url(self):
         return reverse('post-detail', kwargs={'pk': self.pk})
+
+    # def save(self):
+    #     super().save()
+
+    #     img = Image.open(self.image.path)
+
+    #     if image.height > 300 or image.width > 300:
+    #         output_size = (300, 300)
+    #         img.thumbnail(output_size)
+    #         img.save(self.image.path)
